5c0f7367e2ab9ad26d9201f8ae880ac11eb20147
nurlanssu <nurlanssu@code.edu.az>
2026-07-22 10:40:24 +0400

    added foreach

---
+++ Nurlan Suleymanov.foreach/Program.cs
@@ -0,0 +1,288 @@
+﻿#region task1
+// Ədədlər massivindəki bütün elementləri faktorialına yüksəldin (məs: {5, 4, 6} -> {120, 24, 720 })
+//int[] arr = { 3, 4, 5, 6 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    int factorial = 1;
+//    for (int j = 1; j <= arr[i]; j++)
+//    {
+//        factorial *= j;
+//    }
+//    arr[i] = factorial;
+//}
+//foreach (int arr2 in arr)
+//{
+//    Console.WriteLine(arr2);
+//}
+#endregion
+#region task2
+// Ədədlər massivində massivdə olan ən kiçik ədədi çıxardın (məs: {7, 12, 5, 8} -> {7, 12, 8})
+//int[] arr = { 6, 8, 2, 7, 6, 1 };
+//int min = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (min > arr[i])
+//    {
+//        min = arr[i];
+//    }
+//}
+//int[] num = new int[arr.Length - 1];
+//int j = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (min != arr[i])
+//    {
+//        num [j] = arr[i];
+//        j++;
+//    }
+//}
+//foreach (var item in num)
+//{
+//    Console.WriteLine(item);
+//}
+#endregion
+#region task3
+//Verilmiş stringdə yalnız 1 dəfə işlənmiş ilk simvolu çıxışa ver. (məs: "kərtənkələ" -> 'r')
+//string metin = "sensasiyonale";
+
+//for (int i = 0; i < metin.Length; i++)
+//{
+//    int say = 0;
+
+//    for (int j = 0; j < metin.Length; j++)
+//    {
+//        if (metin[i] == metin[j])
+//        {
+//            say++;
+//        }
+//    }
+
+//    if (say == 1)
+//    {
+//        Console.WriteLine(metin[i]);
+//        break;
+//    }
+//}
+#endregion
+#region task 4
+//N və M ədədi veriləcək. o aralıqda olan polindrom ədədləri tapıb çıxışa verən proqram 
+//(tərsinə və düzünə oxunuşu eyni olan ədədlərə polindrom deyilir. məs: 121, 343, 77, 2552)
+//int n = 2;
+//int m = 200;
+//for (int i = n; i <  m; i++)
+//{
+//    int eded = i;
+//    int tersi = 0;
+//    while (eded > 0)
+//    {
+//        int qaliq = eded % 10;
+//        tersi = tersi * 10 + qaliq;
+//        eded /= 10;
+//    }
+//    if (tersi == i)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+#region task5
+//Verilmiş stringdə ən çox təkrarlanan simvolu və neçə dəfə təkrarlandığını çıxışa verən proqram.
+//string text = "kertenkele";
+
+//char enCoxSimvol = text[0];
+//int maxSay = 0;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    int say = 0;
+
+//    for (int j = 0; j < text.Length; j++)
+//    {
+//        if (text[i] == text[j])
+//        {
+//            say++;
+//        }
+//    }
+
+//    if (say > maxSay)
+//    {
+//        maxSay = say;
+//        enCoxSimvol = text[i];
+//    }
+//}
+
+//Console.WriteLine(enCoxSimvol);
+//Console.WriteLine(maxSay);
+#endregion
+#region task6
+//Verilmiş yazıdan içində boşluqlar olmayan yeni bir yazı düzəltmək 
+//string text = "Salam Nurlan necesen";
+//string yeniText = "";
+
+//foreach (char herf in text)
+//{
+//    if (herf != ' ')
+//    {
+//        yeniText += herf;
+//    }
+//}
+
+//Console.WriteLine(yeniText);
+#endregion
+#region task8
+//Verilmiş yazılar siyahısında verilmiş hərfdən neçə ədəd olduğunu tapan proqram
+//string[] sozler = { "Nurlan", "Kamal", "Fidan" };
+//char herf = 'a';
+
+//int say = 0;
+
+//foreach (string soz in sozler)
+//{
+//    foreach (char simvol in soz)
+//    {
+//        if (simvol == herf)
+//        {
+//            say++;
+//        }
+//    }
+//}
+
+//Console.WriteLine(say);
+#endregion
+#region task9
+//Verilmiş yazının əvvəlindəki boşluqlar silinmiş bir string düzəldən proqram 
+//(Misalçün verilmiş yazı "   salam necesen?   "-dirsə yeni düzələcək string "salam necəsən?   " olmalıdır
+//string text = "   salam necesen?   ";
+//string yeniText = "";
+
+//int basla = 0;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (text[i] != ' ')
+//    {
+//        basla = i;
+//        break;
+//    }
+//}
+
+//for (int i = basla; i < text.Length; i++)
+//{
+//    yeniText += text[i];
+//}
+
+//Console.WriteLine(yeniText);
+#endregion
+#region task10
+//Verilmiş 2 ədəd üzərində verilmiş simvola uyğun əməliyyat edən proqram. 
+//Simvol +,-,* və ya / olmalıdır, əks halda simvolu yenidən daxil edilməsi istənilməlidir.
+//(Misaçün simvol * olarsa verilmiş 2 ədəd vurulacaq və nəticəsi göstəriləcək)
+//int a = 10;
+//int b = 5;
+
+//char emel;
+
+//Console.Write("Simvol daxil et (+, -, *, /): ");
+//emel = Convert.ToChar(Console.ReadLine());
+
+//while (emel != '+' && emel != '-' && emel != '*' && emel != '/')
+//{
+//    Console.Write("Yanlış simvol! Yenidən daxil et: ");
+//    emel = Convert.ToChar(Console.ReadLine());
+//}
+
+//switch (emel)
+//{
+//    case '+':
+//        Console.WriteLine(a + b);
+//        break;
+
+//    case '-':
+//        Console.WriteLine(a - b);
+//        break;
+
+//    case '*':
+//        Console.WriteLine(a * b);
+//        break;
+
+//    case '/':
+//        Console.WriteLine(a / b);
+//        break;
+//}
+#endregion
+#region task 11
+//Verilmiş yazının içində A hərfinin olub olmadığını tapan proqram
+//string text = "Salam Azer";
+//bool tapildi = false;
+
+//foreach (char herf in text)
+//{
+//    if (herf == 'A')
+//    {
+//        tapildi = true;
+//        break;
+//    }
+//}
+
+//if (tapildi)
+//{
+//    Console.WriteLine("A herfi var");
+//}
+//else
+//{
+//    Console.WriteLine("A herfi yoxdur");
+//}
+#endregion
+#region task 12
+//Verilmiş yazıda neçə A hərfinin olduğunu tapan proqram
+//string text = "SALAM AZER";
+//int say = 0;
+
+//foreach (char herf in text)
+//{
+//    if (herf == 'A')
+//    {
+//        say++;
+//    }
+//}
+
+//Console.WriteLine(say);
+#endregion
+#region task13
+//int n = -16;
+
+//while (n <= 0 || n % 2 != 0)
+//{
+//    Console.WriteLine("Eded musbet ve cut deyil");
+
+//    }
+
+//Console.WriteLine(n * n);
+#endregion
+#region task14
+//Verilmiş təhsil növü dəyərinə əsasən o təhsilin saatını göstərən proqram.
+//Əgər verilmiş dəyər "programming"dirsə console-da 400 saat, "design"dırsa 250 saat, "system"dirsə 200 saat, heç biri deyilsə "təhsil novu yanlisdir" yazılmalıdır console-da.
+//string tehsil = "programming";
+
+//switch (tehsil)
+//{
+//    case "programming":
+//        Console.WriteLine("400 saat");
+//        break;
+
+//    case "design":
+//        Console.WriteLine("250 saat");
+//        break;
+
+//    case "system":
+//        Console.WriteLine("200 saat");
+//        break;
+
+//    default:
+//        Console.WriteLine("tehsil novu yanlisdir");
+//        break;
+//}
+#endregion
+
+
+
---
+++ Nurlan Suleymanov/Program.cs
@@ -0,0 +1,192 @@
+﻿
+#region task1
+// 1 ile 100 araligindaki cüt ededlerin cemini tapan algorithm
+//int a = 0;
+//for (int i = 0; i <= 100; i++)
+//{
+//    if (i % 2 == 0)
+//        a += i;
+//}
+//Console.WriteLine(a);
+#endregion
+#region task2
+//Verilmish ededin sade ve ya murekkeb oldugunu tapan alqoritm
+//int n = 7;
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (n % 3 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (n % 4 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (n % 5 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (n % 6 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else
+//{
+//    Console.WriteLine("sade");
+//}
+
+//burdan yuxarida olan task 2 ucun cavab menim elediyimdir etdikden sinra AI ye yoxladiram her defe ki bax gore men duz ediremmi
+//ve ya daha yaxsi yolu varmi burda AI men bu usul uzundu asan usulu var deyib asagida qoydugum algoritmi verdi amma anlaya bilmediim
+//int n = 7;
+//int say = 0;
+
+//for (int i = 2; i < n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        say++;
+//    }
+//}
+
+//if (say == 0)
+//{
+//    Console.WriteLine("Sadedir");
+//}
+//else
+//{
+//    Console.WriteLine("Murekkebdir");
+//}
+#endregion
+#region task3
+//Verilmish ededin mertebelerini ekrana cap eden alqoritm
+//int n = 454515885;
+//int cont = 0;
+//while (n > 0)
+//{
+//    n = n / 10;
+//    cont++;
+//}
+//Console.WriteLine(cont);
+#endregion
+#region task4
+//Verilmish ededin 2 quvveti olub olmadigini tapan alqoritm
+//int n = 31;
+//int q = 0;
+//while (n > 1)
+//{
+//    if (n % 2 == 1)
+//    {
+//        q++;
+//    }
+//    n = n / 2;
+//}
+//if (q == 0)
+//{
+//    Console.WriteLine("2 quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2 quvveti deyil");
+//}
+//// AI den qismen komek aldim mentiqi izzah eledi mene
+#endregion
+#region task5
+//1 - dən M - ədək ədədlər içərisində 3 - ə bölünən ədədlərin sayını tapan alqoritm
+//int M = 50;
+//int j = 0;
+//for (int i = 1; i < M; i++)
+//{
+//    if (i % 3 == 0)
+//    {
+//        j++;
+//    }
+//}
+//Console.WriteLine(j);
+#endregion
+#region task6
+//Verilmiş n ədədinin bölənlərini tapın
+//int n = 20;
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n%i==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+#region task7
+//Sizə x və y tam ədədləri verilir. Siz 0 ilə x arasında yerləşən və y-ə tam bölünən ən böyük ədədi tapmalısınız.
+//Əgər belə bir ədəd yoxdursa, ekranda -1 çap olunmalıdır.
+//int x = 50;
+//int y = 2;
+//int z = -1;
+//for (int i = 1; i < x; i++)
+//{
+//    if (i % y == 0)
+//        z = i;
+//    {
+
+
+//    }
+
+//}
+//Console.WriteLine(z);
+//NOT:taskda qeyd etmisiniz ki 0 ile x arasinda yerlesen yeni for u men 0dan baslatsam ve y nece veriremse verim  0 a bolmede 0 aliram deye  
+// -1 cavabi hec vaxt ala bilmirem. yerin  ki taskda 1 ile x arasi qey etmek istemisiniz
+#endregion
+#region task8
+//Verilmiş 3 rəqəmli ədədin rəqəmlərinin hasilini tapan alqoritm.
+//Misalçün alqoritmə 231 ədədini daxil etdikdə 2*3*1 in nəticəsini verməlidir.
+//int n = 586;
+
+//int t = n % 10;
+//int o = (n / 10) % 10;
+//int y = n / 100;
+
+//int m = t * o * y;
+
+//Console.WriteLine(m);
+#endregion
+#region task9
+//Supermarketdə belə bir aksiya keçirilir: 3 məhsul aldıqda ən ucuz məhsul pulsuz olur.
+//Misalçün 40 , 50 və 20 manatlıq mıəhsullar alsam ümumilikdə 40+50 manat ödənilir.
+//3 Mıhsulun qiyməti verildikdə müştərinin nə qədər ödəməli olduğunu tapan alqoritm
+//int a = 40;
+//int b = 50;
+//int c = 20;
+//if (a < b && a < c)
+//{
+//    Console.WriteLine(b + c);
+//}
+//else if (b < a && b < c)
+//{
+//    Console.WriteLine(a + c);
+//}
+//else
+//{
+//    Console.WriteLine(a+b);
+//}
+#endregion
+#region task10
+//X, Y, Z  ədədləri verilib. Bu ədədlərin ən kiçiyini tapan alqoritm qurun
+//int x = 55;
+//int y = 16;
+//int z = 7;
+//if (x < y && x < z)
+//{
+//    Console.WriteLine(x);
+//}
+//else if (y < x && y < z)
+//{
+//    Console.WriteLine(y);
+//}
+//else
+//{
+//    Console.WriteLine(z);
+//}
+#endregion
+string Nurlan = "Yxsh telebe";
+
+
